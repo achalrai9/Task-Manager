737bc2a1f0dca0a777b075b7a8952467bfa75940
Achal Kumar Rai <achalkumarrai81@gmail.com>
2023-12-28 16:46:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const tasks = document.querySelector('.task-container');
+const input = document.querySelector('.add-task');
+const addTask = document.querySelector('.btn');
+let deleteT = document.querySelectorAll('.delete');
+console.log(deleteT);
+
+const taskAdder = function() {
+    const taskInfo = input.value;
+
+    if(taskInfo != '') {
+        const newTaskGrid = document.createElement('div');
+        newTaskGrid.classList.add('task');
+        const newTask = document.createElement('div');
+        newTask.classList.add('task-text');
+        const deleteTask = document.createElement('div');
+        deleteTask.classList.add('task-text');
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete');
+        console.log(deleteBtn.classList.value);
+        deleteBtn.appendChild(document.createTextNode('Delete'));
+
+        const text = document.createTextNode(taskInfo);
+
+        newTaskGrid.appendChild(newTask);
+        newTaskGrid.appendChild(deleteTask);
+        deleteTask.appendChild(deleteBtn);
+        newTask.appendChild(text);
+        tasks.appendChild(newTaskGrid);
+    }
+
+    else alert('Please add some text');
+
+    deleteT = document.querySelectorAll('.delete');
+
+    deleteT[deleteT.length - 1].addEventListener('click', () => {
+        console.log(deleteT[deleteT.length - 1].parentNode.parentNode);
+        deleteT[deleteT.length - 1].parentNode.parentNode.remove();
+    });
+
+    console.log(deleteT);
+};
+
+addTask.addEventListener('click', taskAdder);
+
+// To delete existing tasks
+
+deleteT.forEach((element) => {
+    element.addEventListener('click', () => {
+        console.log(element.parentNode.parentNode);
+        element.parentNode.parentNode.remove();
+    });
+});
